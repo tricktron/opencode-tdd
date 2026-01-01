9e205d3e530982c9c0678ac63098249eb6e2354f
Thibault Gagnaux <tgagnaux@gmail.com>
2026-01-01 21:25:57 +0100

    Init flake with dev shell
    Category: none

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+    description               = "opencode-tdd";
+    inputs.nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.nix-github-actions =
+    {
+        url                    = "github:nix-community/nix-github-actions";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = { self, nixpkgs, nix-github-actions, ... }:
+    let
+        supportedSystems =
+        [
+            "x86_64-darwin"
+            "aarch64-darwin"
+            "x86_64-linux"
+        ];
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+        devShells = forAllSystems
+        (system:
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            default = pkgs.mkShell
+            {
+                buildInputs = with pkgs;
+                [
+                    nodejs
+                    bun
+                    eslint
+                    typescript
+                    prettier
+                ];
+            };
+        });
+    };
+}
